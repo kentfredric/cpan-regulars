0de2d46399875d78ea113c8824d8c4514d86480c
Kent Fredric <kentfredric@gmail.com>
2014-07-29 13:38:48 +1200

    move time_to_bins to regulars library

--- lib/regulars.pm
+++ lib/regulars.pm
@@ -8,7 +8,7 @@ package regulars;
 
 # AUTHORITY
 
-use Sub::Exporter::Progressive -setup => { exports => [qw( final_week_number )] };
+use Sub::Exporter::Progressive -setup => { exports => [qw( final_week_number time_to_bins )] };
 use DateTime;
 
 sub final_week_number
@@ -22,6 +22,28 @@ sub final_week_number
     }
 
     return $final_week_number{$year};
+}
+
+sub time_to_bins
+{
+    my $time        = shift;
+    my @ts          = gmtime($time);
+    my $year        = $ts[5] + 1900;
+    my $month       = $ts[4] + 1;
+    my $day         = $ts[3];
+    my $date        = sprintf('%.4d-%.2d-%.2d', $year, $month, $day);
+    my $ym          = sprintf('%.4d-%.2d', $year, $month);
+    my $dt          = DateTime->from_epoch(epoch => $time);
+    my $week_number = $dt->strftime('%U');
+    my $week_year   = $year;
+
+    if ($week_number == 0) {
+        $week_year--;
+        $week_number = final_week_number($week_year);
+    }
+    my $week = sprintf("%.4d-W%.2d", $week_year, $week_number);
+
+    return ($ym, $week, $date);
 }
 
 1;
